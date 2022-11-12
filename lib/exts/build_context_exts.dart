0eb49688d7cd33108c9bed670c2da94cbd7dfb5b
İhsan Işık <git@ihsan.dev>
2022-11-12 16:44:57 +0100

    Add context.media ext

--- lib/exts/build_context_exts.dart
+++ lib/exts/build_context_exts.dart
@@ -2,5 +2,6 @@ import 'package:flextensions/flextensions.dart';
 import 'package:flutter/material.dart';
 
 extension BuildContextExts on BuildContext {
+  MediaQueryData get media => MediaQuery.of(this);
   ColorScheme get colors => theme.colorScheme;
 }
