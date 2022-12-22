c1bf1fee7356018af5875a4f44cc32db7f2df3e1
İhsan Işık <git@ihsan.dev>
2022-12-22 13:38:22 +0100

    Add `context.findPosition()` ext

--- lib/exts/build_context_exts.dart
+++ lib/exts/build_context_exts.dart
@@ -4,4 +4,9 @@ import 'package:flutter/material.dart';
 extension BuildContextExts on BuildContext {
   MediaQueryData get media => MediaQuery.of(this);
   ColorScheme get colors => theme.colorScheme;
+
+  Offset findPosition() {
+    final RenderBox box = findRenderObject() as RenderBox;
+    return box.localToGlobal(Offset.zero);
+  }
 }
