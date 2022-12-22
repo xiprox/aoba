dfb0ce3c0d028a6daf03007dac17945775a64935
İhsan Işık <git@ihsan.dev>
2022-12-22 16:26:04 +0100

    Fix `run` commands after fvm

--- run.bat
+++ run.bat
@@ -3,8 +3,8 @@
 @REM Runs code generation. Use: "./run.bat gen" or "./run.bat gen watch" in project dir.
 if "%1" == "gen" (
     if "%2" == "watch" (
-        flutter pub run build_runner watch --delete-conflicting-outputs
+        fvm flutter pub run build_runner watch --delete-conflicting-outputs
     ) else (
-        flutter pub run build_runner build --delete-conflicting-outputs
+        fvm flutter pub run build_runner build --delete-conflicting-outputs
     )
 )
