beee5db0901c592cd3130da94fa4f22cef0f3922
İhsan Işık <git@ihsan.dev>
2022-12-28 23:37:19 +0100

    Add some more GQL type aliases and exts

--- lib/data/model/aliases.dart
+++ lib/data/model/aliases.dart
@@ -1,3 +1,8 @@
 import 'package:aoba/data/remote/gql/schema/schema.gql.dart';
 
 typedef MediaType = Enum$MediaType;
+typedef MediaFormat = Enum$MediaFormat;
+typedef MediaStatus = Enum$MediaStatus;
+
+typedef ScoreFormat = Enum$ScoreFormat;
+typedef MediaListStatus = Enum$MediaListStatus;
--- lib/data/model/extensions.dart
+++ lib/data/model/extensions.dart
@@ -4,7 +4,7 @@ import 'aliases.dart';
 
 /// Extensions on alises for generated GraphQL types.
 
-extension MediaTypeExtension on MediaType {
+extension MediaTypeExts on MediaType {
   ImageType? toImageType() {
     switch (this) {
       case MediaType.ANIME:
@@ -13,6 +13,65 @@ extension MediaTypeExtension on MediaType {
         return ImageType.book;
       case MediaType.$unknown:
         return null;
+    }
+  }
+
+  String get displayName {
+    switch (this) {
+      case MediaType.ANIME:
+        return 'Anime';
+      case MediaType.MANGA:
+        return 'Manga';
+      case MediaType.$unknown:
+        return 'Unknown';
+    }
+  }
+}
+
+extension MediaFormatExts on MediaFormat {
+  String get displayName {
+    switch (this) {
+      case MediaFormat.TV:
+        return 'TV';
+      case MediaFormat.TV_SHORT:
+        return 'TV Short';
+      case MediaFormat.MOVIE:
+        return 'Movie';
+      case MediaFormat.SPECIAL:
+        return 'Special';
+      case MediaFormat.OVA:
+        return 'OVA';
+      case MediaFormat.ONA:
+        return 'ONA';
+      case MediaFormat.MUSIC:
+        return 'Music';
+      case MediaFormat.MANGA:
+        return 'Manga';
+      case MediaFormat.NOVEL:
+        return 'Novel';
+      case MediaFormat.ONE_SHOT:
+        return 'One-shot';
+      case MediaFormat.$unknown:
+        return 'Unknown';
+    }
+  }
+}
+
+extension MediaStatusExts on MediaStatus {
+  String get displayName {
+    switch (this) {
+      case MediaStatus.FINISHED:
+        return 'Finished';
+      case MediaStatus.RELEASING:
+        return 'Releasing';
+      case MediaStatus.NOT_YET_RELEASED:
+        return 'Not yet released';
+      case MediaStatus.CANCELLED:
+        return 'Cancelled';
+      case MediaStatus.HIATUS:
+        return 'On Hiatus';
+      case MediaStatus.$unknown:
+        return 'Unknown';
     }
   }
 }
