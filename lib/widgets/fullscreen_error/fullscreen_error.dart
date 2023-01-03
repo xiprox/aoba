92266900854c38b79b23f195cc3c1384b571469a
İhsan Işık <git@ihsan.dev>
2023-01-03 15:47:39 +0100

    Make fullscreen error show snackbar with stack trace on long press

--- lib/widgets/fullscreen_error/fullscreen_error.dart
+++ lib/widgets/fullscreen_error/fullscreen_error.dart
@@ -2,8 +2,8 @@ import 'package:aoba/consts/consts.dart';
 import 'package:aoba/data/model/resource.dart';
 import 'package:aoba/exts/build_context_exts.dart';
 import 'package:aoba/exts/material_exts.dart';
-import 'package:aoba/widgets/tint/tint.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class FullscreenError extends StatelessWidget {
   final ErrorInfo error;
@@ -16,35 +16,48 @@ class FullscreenError extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final colors = context.colors;
-    return Center(
-      child: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          ClipRRect(
-            borderRadius: BorderRadius.circular(12),
-            child: Image.asset(
-              'assets/images/notlikethis.jpg',
-              width: 100,
-              height: 100,
-              color: colors.error.manipulate(1).withOpacity(0.8),
-              colorBlendMode: BlendMode.color,
+    return GestureDetector(
+      onLongPress: () {
+        if (error.stackTrace != null) {
+          HapticFeedback.lightImpact();
+          ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(
+              content: Text(error.stackTrace.toString()),
+              duration: const Duration(seconds: 10),
             ),
-          ),
-          const SizedBox(height: 16),
-          Text(
-            'あの。。。',
-            style: TextStyle(
-              fontSize: 18,
-              fontWeight: FontWeight.w500,
-              color: colors.error,
+          );
+        }
+      },
+      child: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            ClipRRect(
+              borderRadius: BorderRadius.circular(12),
+              child: Image.asset(
+                'assets/images/notlikethis.jpg',
+                width: 100,
+                height: 100,
+                color: colors.error.manipulate(1).withOpacity(0.8),
+                colorBlendMode: BlendMode.color,
+              ),
             ),
-          ),
-          const SizedBox(height: 8),
-          Text(
-            error.message ?? kDefaultUnknownErrorText,
-            style: TextStyle(color: colors.onBackground),
-          ),
-        ],
+            const SizedBox(height: 16),
+            Text(
+              'あの。。。',
+              style: TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.w500,
+                color: colors.error,
+              ),
+            ),
+            const SizedBox(height: 8),
+            Text(
+              error.message ?? kDefaultUnknownErrorText,
+              style: TextStyle(color: colors.onBackground),
+            ),
+          ],
+        ),
       ),
     );
   }
