525892590ca9a95a08dc3a6b0ef7b891113c3599
İhsan Işık <git@ihsan.dev>
2023-05-12 20:59:50 +0200

    Use `Duration.refreshPeriod()` in `Timestamp`

--- lib/widgets/feed/activity/common/timestamp.dart
+++ lib/widgets/feed/activity/common/timestamp.dart
@@ -1,4 +1,5 @@
 import 'package:aoba/exts/build_context_exts.dart';
+import 'package:aoba/exts/duration_exts.dart';
 import 'package:date_time_format/date_time_format.dart';
 import 'package:flutter/material.dart';
 import 'package:timer_builder/timer_builder.dart';
@@ -14,17 +15,8 @@ class Timestamp extends StatelessWidget {
 
     final fromNow = DateTime.now().difference(date).abs();
 
-    Duration refreshPeriod = const Duration(days: 1000);
-    if (fromNow.inMinutes < 1) {
-      refreshPeriod = const Duration(seconds: 1);
-    } else if (fromNow.inHours < 1) {
-      refreshPeriod = const Duration(minutes: 1);
-    } else if (fromNow.inDays < 1) {
-      refreshPeriod = const Duration(minutes: 20);
-    }
-
     return TimerBuilder.periodic(
-      refreshPeriod,
+      fromNow.refreshPeriod(),
       builder: (context) {
         return Text(
           date.relative(
