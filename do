deae50be01ecb6a70a27b2498846955d69c4583f
İhsan Işık <git@ihsan.dev>
2023-10-06 15:06:39 +0200

    Update do script comments

--- do
+++ do
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-# Runs code generation. Use: "./gen" or "./gen watch" in project dir.
+# Runs code generation. Use: "./do gen" or "./do gen watch" in project dir.
 if [[ $1 == "gen" ]]; then
     if [[ $2 == "watch" ]]; then
         fvm flutter pub run build_runner watch --delete-conflicting-outputs
--- do.bat
+++ do.bat
@@ -1,6 +1,6 @@
 @echo off
 
-@REM Runs code generation. Use: "./run.bat gen" or "./run.bat gen watch" in project dir.
+@REM Runs code generation. Use: "./do.bat gen" or "./do.bat gen watch" in project dir.
 if "%1" == "gen" (
     if "%2" == "watch" (
         fvm flutter pub run build_runner watch --delete-conflicting-outputs
