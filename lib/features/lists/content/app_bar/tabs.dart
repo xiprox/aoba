740c1fc4aa368f66898505d4e7e5023ca5bbb403
İhsan Işık <git@ihsan.dev>
2023-11-03 21:13:36 +0100

    Fix undesiderd spacing in lists tabbar

--- lib/features/lists/content/app_bar/tabs.dart
+++ lib/features/lists/content/app_bar/tabs.dart
@@ -15,31 +15,27 @@ class ListsTabs extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final colors = context.colors;
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.stretch,
-      children: [
-        TabBar(
-          controller: controller,
-          isScrollable: true,
-          labelPadding: const EdgeInsets.symmetric(
-            horizontal: 16,
-            vertical: 12,
-          ),
-          splashBorderRadius: const BorderRadius.all(
-            Radius.circular(8),
-          ),
-          indicator: UnderlineTabIndicator(
-            borderRadius: BorderRadius.circular(16),
-            borderSide: BorderSide(color: colors.primary, width: 2),
-          ),
-          dividerColor: Colors.transparent,
-          tabs: lists
-              .map(
-                (it) => Text(it?.name ?? '?', key: ValueKey(it.hashCode)),
-              )
-              .toList(),
-        ),
-      ],
+    return TabBar(
+      controller: controller,
+      isScrollable: true,
+      tabAlignment: TabAlignment.start,
+      labelPadding: const EdgeInsets.symmetric(
+        horizontal: 16,
+        vertical: 12,
+      ),
+      splashBorderRadius: const BorderRadius.all(
+        Radius.circular(8),
+      ),
+      indicator: UnderlineTabIndicator(
+        borderRadius: BorderRadius.circular(16),
+        borderSide: BorderSide(color: colors.primary, width: 2),
+      ),
+      dividerColor: Colors.transparent,
+      tabs: lists
+          .map(
+            (it) => Text(it?.name ?? '?', key: ValueKey(it.hashCode)),
+          )
+          .toList(),
     );
   }
 }
